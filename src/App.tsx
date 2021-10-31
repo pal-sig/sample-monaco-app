b2a8f1313fa809b08c799c3b2f834d4394a8a3e7
FIPalash Gupta <palash@signoz.io>
2021-10-31 17:33:28 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as monaco from "monaco-editor";
+import { useRef, useEffect, useCallback, useState } from "react";
 
 function App() {
+  const divEl = useRef<HTMLDivElement>(null);
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
+  const value = useRef(`groups:
+- name: ExampleCPULoadGroup
+  rules:
+  - alert: HighCpuLoad
+    expr: system_cpu_load_average_1m > 0.1
+    for: 0m
+    labels:
+      severity: warning
+    annotations:
+      summary: High CPU load
+      description: "CPU load is > 0.1\n  VALUE = {{ $value }}\n  LABELS = {{ $labels }}"`);
+
+  useEffect(() => {
+    let editor = editorRef.current;
+
+    if (divEl.current) {
+      editor = monaco.editor.create(divEl.current, {
+        value: value.current,
+        useShadowDOM: true,
+        minimap: {
+          enabled: false,
+        },
+        language: "yaml",
+      });
+    }
+
+    editor?.getModel()?.onDidChangeContent(() => {
+      value.current = editor?.getValue() || "";
+    });
+    return () => {
+      if (editor) {
+        editor.dispose();
+      }
+    };
+  }, [value]);
+
+  const onSaveHandler = useCallback(() => {
+    console.log(value.current);
+  }, [value]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ minHeight: "50vh" }} ref={divEl}></div>
+
+      <button onClick={onSaveHandler}>Save</button>
+    </>
   );
 }
 
